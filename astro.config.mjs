65e52aabff970c7b6af7896e2c29e5b1c9e728f3
Luiz Campos <campos.antoonio@gmail.com>
2024-09-27 18:47:20 -0300

    feat: possible last astrojs commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react({experimentalReactChildren: true})],
+  integrations: [tailwind(), svelte()],
   markdown: {
     shikiConfig: {
       // https://shiki.style/themes
