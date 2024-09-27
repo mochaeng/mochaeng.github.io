5d9f31ba1370cf89fb8d4ff5c867179f2c774452
Luiz Campos <campos.antoonio@gmail.com>
2024-09-27 20:54:59 -0300

    fix: build path

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -19,7 +19,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			// base: process.argv.includes('dev') ? '' : '/mochaeng.github.io'
+			base: process.argv.includes('dev') ? '' : ''
 		}
 	}
 };
